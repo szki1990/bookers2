d68f31e8f1ce6d04eeeaeb05241db1cfcb10a281
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-05-05 09:10:42 +0000

    [Add]ユーザー画像

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :post_images, dependent: :destroy
+  #has_many :post_images, dependent: :destroy
   
-  has_one_attached :profile_image
+  #has_one_attached :profile_image
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/NoImage.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
+    end 
+      image
+  end 
   
   def get_profile_image
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      file_path = Rails.root.join('app/assets/images/NoImage.jpeg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end 
     profile_image.variant(resize_to_limit: [100,100]).processed
