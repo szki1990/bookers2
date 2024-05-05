e0b46f165b2498231cc49cde82e82b3384f1d536
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-05-05 13:06:34 +0000

    [Add]バリデーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,16 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You have updated user successfully."
+      redirect_to :new_book_path
+    else
+      render :edit
+    end
   end 
   
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
          
   #has_many :post_images, dependent: :destroy
   
-  #has_one_attached :profile_image
+  has_one_attached :profile_image
+  
+  validates :name, presence: true
+
   
   def get_image
     unless image.attached?
