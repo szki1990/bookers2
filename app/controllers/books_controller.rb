4ae5f61f91ae0c88773d1bc9d047d23b6a8025f9
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-05-07 02:12:58 +0000

    [Add]編集、削除リンク作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,16 @@ class BooksController < ApplicationController
       render :index
     end 
   end 
+  
+  def edit
+    @book = Book.find(params[:id])
+  end 
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end 
 
   def index
    
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :edit, :show, :destroy, :update]
   resources :users, only: [:show, :edit, :update]
   
   get "homes/about" => "homes#about"
